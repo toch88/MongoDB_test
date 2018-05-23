df705340bd7223ecb8187d241986c4cd374003e1
toch88 <tocha.mateusz@gmail.com>
2018-05-23 13:35:48 +0200

    Upgrade of adding the data, beforeEach test drop collection

--- test/connection.js
+++ test/connection.js
@@ -12,4 +12,12 @@ before((done) => {
     }).on('error', (error) => {
         console.log('Connection error: ', error);
     });
+});
+
+// Drop the characteres collection before each test
+beforeEach((done)=>{
+    mongoose.connection.collections.mariochars.drop(()=>{
+        done();
+    })
 })
+
